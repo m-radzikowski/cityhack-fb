4f2c3ec5ed0f19ce5734a5144a00890063efaf48
Maciej Radzikowski <mradzikowski@blastlab.co>
2018-06-25 12:28:43 +0200

    Init

---
+++ src/main/java/co/blastlab/cityhack/fb/App.java
@@ -0,0 +1,13 @@
+package co.blastlab.cityhack.fb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+}
